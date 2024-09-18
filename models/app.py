28bb3c5e8f9c9402699a513261409f0ec0b6f4be
julio cesar martins <juliocesar1316@gmail.com>
2024-09-18 20:12:54 -0300

    fix-bug/spc-14 correção da conexao com o banco de dados

--- models/app.py
+++ models/app.py
@@ -8,28 +8,18 @@ mongo = PyMongo()
 def create_app():
     app = Flask(__name__)
 
-    mongo_username = os.environ.get('MONGODB_USERNAME', 'spcFlask')
-    mongo_password = os.environ.get('MONGODB_PASSWORD', 'admin')
-    mongo_hostname = os.environ.get('MONGODB_HOSTNAME', 'mongo')
-    mongo_database = os.environ.get('MONGODB_DATABASE', 'spc')
-
-    mongo_uri = f"mongodb://{mongo_username}:{mongo_password}@{mongo_hostname}:27017/spc"
-
-    # client = MongoClient(mongo_uri)
-    # db = client.get_database(mongo_database)
-
-    app.config["MONGO_URI"] = mongo_uri
+    app.config["MONGO_URI"] = f'mongodb://{os.environ.get("MONGODB_HOSTNAME", "mongo")}:27017/{os.environ.get("MONGODB_DATABASE", "spc")}'
     mongo.init_app(app)
 
     # Importar e registrar blueprints (rotas)
-    from .routes.user_routes import userRoute
-    from .routes.term_routes import termRoute
-    from .routes.user_term_routes import userTermRoute
+    from .routes.user_routes import user
+    from .routes.term_routes import term
+    from .routes.user_term_routes import user_term
     # from .routes.verify_routes import verify
 
-    app.register_blueprint(userRoute(), url_prefix='/users')
-    app.register_blueprint(termRoute(), url_prefix='/terms')
-    app.register_blueprint(userTermRoute(), url_prefix='/user_terms')
+    app.register_blueprint(user, url_prefix='/users')
+    app.register_blueprint(term, url_prefix='/terms')
+    app.register_blueprint(user_term, url_prefix='/user_terms')
     # app.register_blueprint(verify, url_prefix='/verify')
 
     return app
--- models/routes/term_routes.py
+++ models/routes/term_routes.py
@@ -3,116 +3,116 @@ from datetime import datetime
 from models.app import mongo
 
 
-def termRoute():
-    term = Blueprint('term', __name__) # Rota utilizada para acesso '/terms'
-    terms_collection = mongo.db.termo # colecao de termos do mongo db
+term = Blueprint('term', __name__) # Rota utilizada para acesso '/terms'
+terms_collection = mongo.db.termo # colecao de termos do mongo db
 
 
-    @term.route('/', methods=['GET'])
-    def hello():
-        return "Hello World!"
+@term.route('/', methods=['GET'])
+def hello():
+    return "Hello World!"
 
 
-    # Rota para criar um termo
-    @term.route('/createTerm', methods=['POST'])
-    def create_term():
-        try:
-            data = request.get_json(force=True)
-            
-            required_fields = ['descricao', 'nome_termo', 'prioridade']
-            for field in required_fields:
-                if field not in data:
-                    return jsonify({'error': f'O campo {field} é obrigatório!'}), 400
-                
-            nome_termo = data['nome_termo'].lower()    
-            
-            versionTerm = 1.0
-            termExists = terms_collection.find_one({'nome_termo': nome_termo})
-            if termExists:
-                versionTerm = termExists['versao'] + 0.1
-            
-            dataTerm = {
-                'nome_termo': nome_termo,
-                'descricao': data['descricao'],
-                'prioridade': data['prioridade'],
-                'data_cadastro': datetime.timestamp(),
-                'versao': versionTerm
-            }
-
-            result = terms_collection.insert_one(dataTerm)
-            if result.inserted_id == 0:
-                return jsonify({'error': 'Termo e Condicoes não inserido!'}), 404
-            
-            return jsonify({'message': 'Termo e Condicoes criado com sucesso!', 'term_id': str(result.inserted_id), **result }), 201
+# Rota para criar um termo
+@term.route('/createTerm', methods=['POST'])
+def create_term():
+    try:
+        data = request.get_json(force=True)
         
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
-
-
-    # Rota para retornar todos os termos ou retorna com filtro de prioridade caso seja passado na rota '/terms?prioridade=1'
-    @term.route('/terms', methods=['GET'])
-    def terms_required():
-        try:
-            prioridade = request.args.get('prioridade', type=int)
-            if prioridade:
-                terms = list(terms_collection.find({'prioridade': prioridade}))
-                if not terms:
-                    return jsonify({'error': f'Termo e Condicoes com prioridade: {prioridade} não encontrado!'}), 404
-            else:
-                terms = list(terms_collection.find())
-
-            terms_json = [{**term, '_id': str(term['_id'])} for term in terms]
-
-            return jsonify(terms_json), 200
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
-        
-
-    # Rota para retornar os termos com o nome e se filtrar se for passado a versão '/<nome_termo>?versao=1.0'
-    @term.route('/<nome_termo>', methods=['GET'])
-    def terms_not_required(nome_termo, versao):
-        try:
-            nome_termo = nome_termo.lower()
-            versao = request.args.get('versao', type=float)
+        required_fields = ['descricao', 'nome_termo', 'prioridade']
+        for field in required_fields:
+            if field not in data:
+                return jsonify({'error': f'O campo {field} é obrigatório!'}), 400
             
-            if versao:
-                terms = list(terms_collection.find({'nome_termo': nome_termo, 'versao': versao}))
-                if not terms:
-                    return jsonify({'error': f'Termo e Condicoes "{nome_termo}" com a versao {versao} não foi encontrado!'}), 400
-            else:
-                terms = list(terms_collection.find({'nome_termo': nome_termo}))
-                if not terms:
-                    return jsonify({'error': f'Termo e Condicoes "{nome_termo}" não foi encontrado!'}), 400
-            
-            terms_json = [{**term, '_id': str(term['_id'])} for term in terms]  
-
-            return jsonify(terms_json), 200
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
+        nome_termo = data['nome_termo'].lower()    
         
+        
+        existing_term = terms_collection.find_one(
+            {'nome_termo': nome_termo},
+            sort=[('versao', -1)]  # Ordenar por versão decrescente
+        )
+        if existing_term:
+            versionTerm = round(existing_term['versao'] + 0.1, 1)
+        else:
+            versionTerm = 1.0    
+        
+        dataTerm = {
+            'nome_termo': nome_termo,
+            'descricao': data['descricao'],
+            'prioridade': data['prioridade'],
+            'data_cadastro': datetime.now().timestamp(),
+            'versao': versionTerm
+        }
 
-    # Rota para retornar os termos sem repeticao e com a versao mais atualizada
-    @term.route('/latestTerm', methods=['GET'])
-    def latest_term():
-        try:        
-            pipeline = [
-                {"$sort": {"nome_termo": 1, "versao": -1}},  
-                {
-                    "$group": {
-                        "_id": "$_id",
-                        "descricao": {"$first": "$descricao"},
-                        "nome_termo": {"$first": "$nome_termo"},
-                        "prioridade": {"$first": "$prioridade"},
-                        "versao": {"$first": "$versao"},
-                        "data_cadastro": {"$first": "$data_cadastro"}
-                    }
+        result = terms_collection.insert_one(dataTerm)      
+        return jsonify({'message': 'Termo e Condicoes criado com sucesso!', 'term_id': str(result.inserted_id), **result }), 201
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# Rota para retornar todos os termos ou retorna com filtro de prioridade caso seja passado na rota '/terms?prioridade=1'
+@term.route('/terms', methods=['GET'])
+def terms_required():
+    try:
+        prioridade = request.args.get('prioridade', type=int)
+        if prioridade:
+            terms = list(terms_collection.find({'prioridade': prioridade}))
+            if not terms:
+                return jsonify({'error': f'Termo e Condicoes com prioridade: {prioridade} não encontrado!'}), 404
+        else:
+            terms = list(terms_collection.find())
+
+        terms_json = [{**term, '_id': str(term['_id'])} for term in terms]
+
+        return jsonify(terms_json), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+
+# Rota para retornar os termos com o nome e se filtrar se for passado a versão '/<nome_termo>?versao=1.0'
+@term.route('/<nome_termo>', methods=['GET'])
+def terms_not_required(nome_termo, versao):
+    try:
+        nome_termo = nome_termo.lower()
+        versao = request.args.get('versao', type=float)
+        
+        if versao:
+            terms = list(terms_collection.find({'nome_termo': nome_termo, 'versao': versao}))
+            if not terms:
+                return jsonify({'error': f'Termo e Condicoes "{nome_termo}" com a versao {versao} não foi encontrado!'}), 400
+        else:
+            terms = list(terms_collection.find({'nome_termo': nome_termo}))
+            if not terms:
+                return jsonify({'error': f'Termo e Condicoes "{nome_termo}" não foi encontrado!'}), 400
+        
+        terms_json = [{**term, '_id': str(term['_id'])} for term in terms]  
+
+        return jsonify(terms_json), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+
+# Rota para retornar os termos sem repeticao e com a versao mais atualizada
+@term.route('/latestTerm', methods=['GET'])
+def latest_term():
+    try:        
+        pipeline = [
+            {"$sort": {"nome_termo": 1, "versao": -1}},  
+            {
+                "$group": {
+                    "_id": "$_id",
+                    "descricao": {"$first": "$descricao"},
+                    "nome_termo": {"$first": "$nome_termo"},
+                    "prioridade": {"$first": "$prioridade"},
+                    "versao": {"$first": "$versao"},
+                    "data_cadastro": {"$first": "$data_cadastro"}
                 }
-            ]
+            }
+        ]
 
-            terms = list(terms_collection.aggregate(pipeline))
+        terms = list(terms_collection.aggregate(pipeline))
 
-            return jsonify(terms), 200
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
+        return jsonify(terms), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-    return term    
