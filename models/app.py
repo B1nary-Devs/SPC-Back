d18fc6af0cde0d38ce001c944683714a01b4d6b2
Noite <juliocesar1316@gmail.com>
2024-09-26 20:48:12 -0300

    fix-bug/spc-35 corrreção do cadastro do usuario

--- models/app.py
+++ models/app.py
@@ -15,11 +15,11 @@ def create_app():
     from .routes.user_routes import user
     from .routes.term_routes import term
     from .routes.user_term_routes import user_term
-    # from .routes.verify_routes import verify
+    from .routes.verify_routes import verify
 
     app.register_blueprint(user, url_prefix='/users')
     app.register_blueprint(term, url_prefix='/terms')
     app.register_blueprint(user_term, url_prefix='/user_terms')
-    # app.register_blueprint(verify, url_prefix='/verify')
+    app.register_blueprint(verify, url_prefix='/verify')
 
     return app
